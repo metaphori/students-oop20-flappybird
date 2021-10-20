176a13530aee98043ecf0117defa8bfefcd0131a
il tuo nomeAriannaZ97 <53905536+AriannaZ97@users.noreply.github.com>
2021-10-20 20:50:20 +0200

    add button in ViewMenu

--- src/main/java/view/ViewMenu.java
+++ src/main/java/view/ViewMenu.java
@@ -2,12 +2,15 @@ package view;
 
 import javax.swing.Renderer;
 
+import controller.Controller;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
 public class ViewMenu {
 
@@ -54,7 +57,7 @@ public class ViewMenu {
         
         
         ImageView img = new ImageView();
-        img.setImage(new Image("background.png"));
+        img.setImage(new Image(ImageID.PLAYING_BACKGROUND.getPath()));
         img.setFitHeight(600);
         img.setFitWidth(800);
         
@@ -64,6 +67,22 @@ public class ViewMenu {
         pane.getChildren().add(playerButton);
         pane.getChildren().add(exitButton);
         
+        
+    }
+    
+    public static void show(Stage parentStage, Controller controller) {
+        Stage stage= new Stage();
+        Label label= new Label("Choose player");
+        Button chooseButton= new Button("Select");
+        label.setStyle("-fx-font-size: 20;");
+        label.setLayoutX(75);
+        label.setLayoutY(20);
+        chooseButton.setLayoutX(120);
+        chooseButton.setLayoutY(120);
+        chooseButton.setPrefWidth(60);
+        
+                
+                
         
     }
 
